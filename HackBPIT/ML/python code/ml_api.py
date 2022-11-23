69f8f52c90b8958c3dd8ea4c54d49a72c693dcf3
lakshya009 <lakshyayadav3009.ly@gmail.com>
2022-11-24 02:57:52 +0530

    ml model api

--- HackBPIT/ML/python code/ml_api.py
+++ HackBPIT/ML/python code/ml_api.py
@@ -12,14 +12,15 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import json
-
+import ssl
+from flask import jsonify
 
 app = FastAPI()
 
 class model_input(BaseModel):
     
     age : int
-    number_of_sexual_partners : int()
+    number_of_sexual_partners : int
     first_sexual_intercourse : float
     num_of_pregnancies : int
     smokes : bool
@@ -36,20 +37,19 @@ class model_input(BaseModel):
     stds_vulvo_perineal_condylomatosis : bool
     stds_genital_herpes : bool
     stds_molluscum_contagiosum : bool
-    stds_vulvo_perineal_condylomatosis : bool
     stds_syphilis : bool
     stds_pelvic_inflammatory_disease : bool
     stds_hiv : bool
     stds_hepatitis_b : bool
     stds_hpv : bool
     stds_number_of_diagnosis : bool
-    stds_cancer : bool
+    dx_cancer : bool
     dx_cin : bool
     dx_hpv : bool
-    dx : bool
-    hinselmann : bool
-    schiller : bool
-    citology : bool
+    # dx : bool
+    # hinselmann : bool
+    # schiller : bool
+    # citology : bool
     
 #importing the saved model
 model = pickle.load(open('model_save.sav', 'rb'))
@@ -63,17 +63,65 @@ def cervical_cancer_prediction(input_parameters: model_input):
     age = input_dictionary['age']
     nsp = input_dictionary['number_of_sexual_partners']
     fsi = input_dictionary['first_sexual_intercourse']
-    fsi = input_dictionary['num_of_pregnancies']
-    fsi = input_dictionary['smokes']
-    fsi = input_dictionary['smokes_years']
-    fsi = input_dictionary['smokes_packs_year']
-    fsi = input_dictionary['hormonal_contraceptives']
-    fsi = input_dictionary['hormonal_contraceptives']
-    fsi = input_dictionary['iud']
-    fsi = input_dictionary['iud_years']
-    fsi = input_dictionary['stds']
-    fsi = input_dictionary['']
-    fsi = input_dictionary['']
+    nop = input_dictionary['num_of_pregnancies']
+    smokes = input_dictionary['smokes'] 
+    sy = input_dictionary['smokes_years']
+    spy = input_dictionary['smokes_packs_year']
+    hc = input_dictionary['hormonal_contraceptives']
+    hcy = input_dictionary['hormonal_contraceptives']
+    iud = input_dictionary['iud']
+    iudy = input_dictionary['iud_years']
+    stds = input_dictionary['stds']
+    stdn = input_dictionary['stds_number']
+    stdc = input_dictionary['stds_condylomatosis']
+    stdvc = input_dictionary['stds_vaginal_condylomatosis']
+    stdvpc = input_dictionary['stds_vulvo_perineal_condylomatosis']
+    sgh = input_dictionary['stds_genital_herpes']
+    smc = input_dictionary['stds_molluscum_contagiosum']
+    ss = input_dictionary['stds_syphilis']
+    spid = input_dictionary['stds_pelvic_inflammatory_disease']
+    sh = input_dictionary['stds_hiv']
+    shb= input_dictionary['stds_hepatitis_b']
+    shpv = input_dictionary['stds_hpv']
+    snod = input_dictionary['stds_number_of_diagnosis']
+    dxc = input_dictionary['dx_cancer']
+    dc = input_dictionary['dx_cin']
+    dh = input_dictionary['dx_hpv']
+    # dx = input_dictionary['dx']
+    # hinsel = input_dictionary['hinselmann']
+    # schiller = input_dictionary['schiller']
+    # citology = input_dictionary['citology']
+# hinsel, schiller, citology
+    input_list = [age, nsp, fsi, nop, smokes, sy, spy, hc, hcy, iud, iudy, stds, stds, stdn, stdc, stdvc, stdvpc, sgh, smc, ss, spid, sh, shb, shpv, snod, dxc, dc, dh]
+
+    prediction = model.predict([input_list]);
+    return prediction[0    ]
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
