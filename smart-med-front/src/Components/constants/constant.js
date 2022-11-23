617b74a45b3067a4e20652bc821807d8bd946b37
Yash Tiwari <yash22012002@gmail.com>
2022-11-24 02:04:57 +0530

    view blockchain records

---
+++ smart-med-front/src/Components/constants/constant.js
@@ -0,0 +1,61 @@
+export const contractAddress = "0x3E1922037F58bd768836F0065f7eCf2CD54C4dB6";
+export const abi = [
+  {
+    inputs: [],
+    name: "get",
+    outputs: [
+      {
+        internalType: "string[]",
+        name: "",
+        type: "string[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "ipfsHash",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "x",
+        type: "string",
+      },
+    ],
+    name: "set",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
